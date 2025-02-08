31adcdeaddf0af9d1e407706cd61646810c947c4
xylophonehero <nick97000@gmail.com>
2025-02-08 12:40:02 -0500

    chore: remove old function

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,4 @@
 import Parser from "tree-sitter";
-import type { Node, SourceFile } from "typescript/lib/tsserverlibrary";
 
 export function removeQuotes(text: string) {
   return text.slice(1, -1);
@@ -29,17 +28,4 @@ export function createNodeDefinition(
       },
     ],
   };
-}
-
-export function findNodeAtPosition(
-  ts: typeof import("typescript/lib/tsserverlibrary"),
-  sourceFile: SourceFile,
-  position: number,
-): Node | undefined {
-  function find(node: Node): Node | undefined {
-    if (position >= node.getStart() && position < node.getEnd()) {
-      return ts.forEachChild(node, find) ?? node;
-    }
-  }
-  return find(sourceFile);
 }
